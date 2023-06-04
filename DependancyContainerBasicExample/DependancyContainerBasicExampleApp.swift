4f9e33717d52a3825d037a4ed59c3edc8ab3ba2f
Pac12Run8000 <gangstarr78992@>
2023-06-03 17:56:25 -0700

    Initial Commit

---
+++ DependancyContainerBasicExample/DependancyContainerBasicExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependancyContainerBasicExampleApp.swift
+//  DependancyContainerBasicExample
+//
+//  Created by Michelle Grover on 6/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DependancyContainerBasicExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
